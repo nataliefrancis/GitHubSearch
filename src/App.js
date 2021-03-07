610914b66120a6078b8de1e3150b01f00e53e18b
Natalie Francis <natdfrancis@gmail.com>
2021-03-07 14:03:56 -0700

    creates routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 //import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+import Search from './search';
+import Table from './table';
+import Details from './details';
+
+class App extends React.Component {
+
   render() {
     return (
-
-    );
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route path="/results" component={Table} />
+          <Route path="/details" component={Details} />
+        </Switch>
+      </Router>
+    )
   }
 
 }
